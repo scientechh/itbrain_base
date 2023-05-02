02a6f53f62544dff263d125266a44a2a81031602
scientechh <112556327+scientechh@users.noreply.github.com>
2023-05-02 19:44:08 +0400

    Create FilterObj.js

---
+++ FilterObj.js
@@ -0,0 +1,32 @@
+const FilterObj = {
+        programming: {
+            Img: "prog",
+            Id: "programming",
+            Color: "programmin-blue",
+            Title: "Վեբ Ծրագրավորում"
+        },
+        uxui: {
+            Img: "uxui",
+            Id: "uxui",
+            Color: "ux-yellow",
+            Title: "UX/UI"
+        },
+        qa: {
+            Img: "qa",
+            Id: "qa",
+            Color: "qa-green",
+            Title: "Որակի Ապահովում"
+        },
+        gd: {
+            Img: "gd",
+            Id: "gd",
+            Color: "gd-red",
+            Title: "Գրաֆիկ Դիզայն"
+        },
+        webka: {
+            Img: "web",
+            Id: "webka",
+            Color: "webka-purple",
+            Title: "Վեբ Մշակում"
+        }
+    }
